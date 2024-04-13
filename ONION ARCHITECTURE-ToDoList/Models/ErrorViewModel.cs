1ce9c78b3b860917a3c54cec167d1ce0be7a8025
Francisco-KevinNico-BSIT32E2 <kevinfrancisco9123@gmail.com>
2024-04-13 11:15:42 +0800

    Add project files.

---
+++ ONION ARCHITECTURE-ToDoList/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ONION_ARCHITECTURE_ToDoList.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
